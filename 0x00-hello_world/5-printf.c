f31d5a5ac6f73b6d200551d9fbaaefb876850d43
UmoloBlessing <umoloblessing009@gmail.com>
2023-06-15 04:28:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+printf("%s\n", "...but the outcome is a piece of art");
+return (0);
+}
