8dcfd044f253fa9f2204f228efc83b4739959736
UmoloBlessing <umoloblessing009@gmail.com>
2023-06-16 05:20:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		putchar(number + '0');
+
+	putchar('\n');
+
+	return (0);
+}
