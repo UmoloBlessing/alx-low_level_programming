f4d32a3889f9c3393ebe16b02672d45e1c12b9f2
UmoloBlessing <umoloblessing009@gmail.com>
2023-06-16 06:15:28 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - all possible combinations of two two-digit numbers.
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int number1;
+	int number2;
+
+	for (number1 = 0; number1 <= 99; number1++)
+	{
+		for (number2 = number1; number2 <= 99; number2++)
+		{
+			putchar('0' + (number1 / 10));
+			putchar('0' + (number1 % 10));
+			putchar(' ');
+			putchar('0' + (number2 / 10));
+			putchar('0' + (number2 % 10));
+
+			if (number1 != 99 || number2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
