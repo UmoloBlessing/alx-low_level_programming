3e7c418b97d70e372d823579d68fa193467dee1f
UmoloBlessing <umoloblessing009@gmail.com>
2023-06-16 06:22:08 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,21 @@ int main(void)
 
 	for (number1 = 0; number1 <= 99; number1++)
 	{
-		for (number2 = number1; number2 <= 99; number2++)
+		for (number2 = 0; number2 <= 99; number2++)
 		{
-			putchar('0' + (number1 / 10));
-			putchar('0' + (number1 % 10));
-			putchar(' ');
-			putchar('0' + (number2 / 10));
-			putchar('0' + (number2 % 10));
-
-			if (number1 != 99 || number2 != 99)
+			if (number1 < number2)
 			{
-				putchar(',');
+				putchar('0' + (number1 / 10));
+				putchar('0' + (number1 % 10));
 				putchar(' ');
+				putchar('0' + (number2 / 10));
+				putchar('0' + (number2 % 10));
+
+				if (number1 != 98 || number2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
