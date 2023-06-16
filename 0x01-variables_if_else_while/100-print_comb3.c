c98271bed525c432967e28fb01c894ec41fa8a51
UmoloBlessing <umoloblessing009@gmail.com>
2023-06-16 07:06:36 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - all possible different combinations of two digits.
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int number1;
+	int number2;
+
+	for (number1 = 0; number1 <= 8; number1++)
+	{
+		for (number2 = number1 + 1; number2 <= 9; number2++)
+		{
+			putchar('0' + number1);
+			putchar('0' + number2);
+
+			if (number1 != 8 || number2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
